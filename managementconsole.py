b56c382aa295ad0f5f86dc94acfd3ac078ed35fc
czs5664 <czs5664@psu.edu>
2018-03-28 22:56:00 +0000

    base code for management console

--- managementconsole.py
+++ managementconsole.py
@@ -4,6 +4,6 @@ def application(environ, start_response):
     start_response("200 OK", [("Content-type", "text/plain")])
     return ["Hello my friend!".encode("utf-8")]
 
-#server to host management console webpage for escape room
+
 server = make_server('localhost', 8080, application)
 server.serve_forever()
